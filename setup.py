6be11497e8971287784541cb8609d7d29a817a13
Björn Schrammel <github@schrammel.dev>
2025-05-02 21:02:21 +0200

    Add LICENSE file and update requirements in setup.py

--- setup.py
+++ setup.py
@@ -10,10 +10,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="",
     packages=setuptools.find_packages(),
-    install_requires=[
-        "bitwarden_sdk",
-        "dotenv"
-    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
